0d57303b92023aef01312f25ae50a1ee993aba1a
Елена Сутковенко < >
2016-06-11 21:15:07 +0300

    Initial commit

---
+++ src/NewPackage/NewJavaClass.java
@@ -0,0 +1,10 @@
+package NewPackage;
+
+/**
+ * Created by 1 on 11.06.2016.
+ */
+public class NewJavaClass {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
